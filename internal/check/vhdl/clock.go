51c7bbfc6b9bf50e6b39a641a2857a6d09f98fce
Michał Kruszewski <mkru@protonmail.com>
2022-03-07 19:20:38 +0100

    [vhdl] Add function for clock port mapping.

---
+++ internal/check/vhdl/clock.go
@@ -0,0 +1,19 @@
+package vhdl
+
+import (
+	"regexp"
+)
+
+var clockPortMapWithFrequenciesRegexp *regexp.Regexp = regexp.MustCompile(`cl(oc)?k_?(\d+).*=>.*cl(oc)?k_?(\d+)`)
+
+func CheckClockPortMapping(line string) (string, bool) {
+	matches := clockPortMapWithFrequenciesRegexp.FindStringSubmatch(line)
+
+	if len(matches) > 0 {
+		if matches[2] != matches[4] {
+			return "clock frequency mismatch", false
+		}
+	}
+
+	return "", true
+}
---
+++ internal/check/vhdl/clock_test.go
@@ -0,0 +1,33 @@
+package vhdl
+
+import (
+	"testing"
+)
+
+func TestCheckClockPortMapping(t *testing.T) {
+	var tests = []struct {
+		line string
+		msg  string
+		ok   bool
+	}{
+		// Invalid mappings
+		{line: "clk_10=>clk_20", msg: "clock frequency mismatch", ok: false},
+		{line: "clk_40_i => clk_160)", msg: "clock frequency mismatch", ok: false},
+		{line: "clk70 => clk_80", msg: "clock frequency mismatch", ok: false},
+		{line: "clk_70 => clk80_i", msg: "clock frequency mismatch", ok: false},
+		{line: "clk70 => clock120", msg: "clock frequency mismatch", ok: false},
+		{line: "clock70_i => clk120_i,", msg: "clock frequency mismatch", ok: false},
+		// Valid mappings
+		{line: "clk70_i => clk70,", msg: "", ok: true},
+		{line: "clk25 => clk_25,", msg: "", ok: true},
+		{line: "clock125 => clk_125,", msg: "", ok: true},
+		{line: "clock_40_i=>clock40,", msg: "", ok: true},
+	}
+
+	for i, test := range tests {
+		msg, ok := CheckClockPortMapping(test.line)
+		if msg != test.msg || ok != test.ok {
+			t.Errorf("[%d]: got (%v, %v); want (%v, %v)", i, msg, ok, test.msg, test.ok)
+		}
+	}
+}
