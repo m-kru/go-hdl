8ef9b39635dddb107a454df0ed775f648ef9faed
Michał Kruszewski <mkru@protonmail.com>
2022-04-04 10:06:04 +0200

    Update doc help.

--- internal/args/doc_help.go
+++ internal/args/doc_help.go
@@ -6,30 +6,13 @@ var docHelpMsg string = `Doc command
 Usage
 -----
 
-  thdl doc [flags] [<language>:][<library>.]<primarySymbol>[.<secondarySymbol>]
+  thdl doc [flags] symbolPath
 
 Flags:
   -debug      Print debug messages.
   -no-bold    Don't print language keywords in bold.
   -no-config  Don't read .thdl.yml config file.
   -fusesoc    Infer libraries from FuseSoc .core files.
-
-Language is one of:
- - VHDL
-
-Language is case insensitive.
-Library, primarySymbol and secondarySymbol obey the case sensitivity of the language.
-
-If language is omitted all languages are searched.
-If multiple symbols are found, then ambiguity is reported.
-
-If path to symbol consists of two words and a dot ("foo.bar"), then two scenarios
-are assumed: library.primarySymbol and primarySymbol.secondarySymbol.
-If multiple symbols are found, then ambiguity is reported.
-
-If path to symbol is a single word, then two scenarios are assumed:
-primarySymbol and secondarySymbol.
-If multiple symbols are found, then ambiguity is reported.
 
 
 Description
@@ -38,7 +21,7 @@ Description
 The doc command prints the documentation comment associated with the symbol
 identified by its arguments followed by the source code of the symbol.
 
-Following kinds of symbol are supported by the doc command:
+The following kinds of symbols are supported by the doc command:
 
   VHDL:
     - constant declaration within package declaration,
@@ -48,4 +31,50 @@ Following kinds of symbol are supported by the doc command:
     - package instantiation within package declaration,
     - procedure declaration within package declaration,
     - type declaration within package declaration.
+
+
+Symbol path
+-----------
+
+The symbol path has following structure:
+
+  language:library.primarySymbol.secondarySymbol.tertiarySymbol
+
+Language is one of:
+ - VHDL
+If language is omitted, all languages are searched.
+
+Language is case insensitive. Library, primarySymbol, secondarySymbol and
+tertiarySymbol obey the case sensitivity of the language. A tertiary symbol
+can't be '*' wildcard.
+
+Symbol path can consist of one to five words.
+If symbol path is a single word three scenarios are assumed:
+  - library,
+  - primarySymbol,
+  - secondarySymbol.
+If symbol path consists of two words and a dot ("foo.bar") three scenarios
+are assumed:
+  - library.primarySymbol,
+  - primarySymbol.secondarySymbol,
+  - secondarySymbol.tertiarySymbol.
+If symbol path consists of three words and two dots ("foo.bar.baz") two
+scenarios are assumed:
+  - library.primarySymbol.secondarySymbol,
+  - primarySymbol.secondarySymbol.tertiarySymbol.
+
+If multiple symbols are found ambiguity is reported.
+
+To resolve symbol path ambiguity extend the path by adding the preceding symbol
+name. If symbols with the same name exist at different levels,  resolve
+ambiguity by adding '.' at the end. For example, let's assume there is "foo"
+library, "foo" primarySymbol, "foo" secondarySymbol, and "foo" tertiarySymbol.
+To see the documentation of "foo" library type:
+  thdl doc foo.
+To see the documentation of "foo" primarySymbol type:
+  thdl doc foo.foo.
+To see the documentation of "foo" secondarySymbol type:
+  thdl doc foo.foo.foo.
+To see the documentation of "foo" tertiarySymbol type:
+  thdl doc foo.foo.foo.foo
 `
